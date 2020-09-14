ce9c7a4550672e5562753d71b72cfff3a1042e41
hoit1302 <jueun000101@gmail.com>
2020-09-14 09:27:03 +0900

    Servlet 3.0 spec미만 (version="2.5")

---
+++ 2-fe/lab/1-2/exam25_04/src/exam/TenServlet.java
@@ -0,0 +1,38 @@
+package exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TenServlet
+ */
+// ������̼��� ���� [������̼�(@, Annotation)]
+public class TenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TenServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8"); // ������ Ÿ�� ����
+		PrintWriter out = response.getWriter(); // ��¥ ���� ������ �־��� ��� ����
+		out.println("<h1>1-10���� ���!!<h1>"); // println print ����� �ʿ� ���� ������ �ȸԾ�
+		for (int i = 1; i <= 10; i++)
+			out.println(i + "<br>");
+		out.close();
+	}
+
+
+}
