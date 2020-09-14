e3b51c90ede9925898a8c1371e62219a20fc877a
hoit1302 <jueun000101@gmail.com>
2020-09-14 12:05:19 +0900

    Swagger

---
+++ 2-be/lab/3-4/calculator06/src/main/java/org/edwith/webbe/calculator/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package org.edwith.webbe.calculator.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"org.edwith.webbe.calculator.service"})
+public class ApplicationConfig {
+}
