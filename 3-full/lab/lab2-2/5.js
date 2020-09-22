a533eb59aea9590395d2e66b8c3b0ada75e97dab
hoit1302 <jueun000101@gmail.com>
2020-09-22 09:25:18 +0900

    JavaScript Debugging

---
+++ 3-full/lab/lab2-2/5.js
@@ -0,0 +1,11 @@
+// 브라우저에서 디버깅을 보통 많이 합니다.
+// todomvc.com에 들어와서 Vanilla JS 페이지
+// 어떤 프레임워크 없이 실행될 수 있는 걸 보통 바닐라 JS라 부름
+
+// breakpoint 걸고 여러가지 확인 가능.
+// 디버거창: call stack / local 등 확인 가능
+// Pause/Continue, step over/step into(코드 라인별 진행/코드 라인별 실행), step out(함수 밖으로 빠져나감)
+
+
+
+// https://developers.google.com/web/tools/chrome-devtools/javascript/?hl=ko
